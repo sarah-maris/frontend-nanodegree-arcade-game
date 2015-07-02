ebb1022caa73cf614d08e2d158b8c050aa494a14
sarah-maris <sarah.87.maris@gmail.com>
2015-07-02 13:48:45 -0400

    Git interface test #2

--- js/app.js
+++ js/app.js
@@ -14,6 +14,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+	
+	
 }
 
 // Draw the enemy on the screen, required method for game
