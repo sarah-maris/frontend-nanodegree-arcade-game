e11dd224f729acceb9d1a7e3d0eca299001c50a0
sarah-maris <sarah.87.maris@gmail.com>
2015-07-03 14:49:50 -0400

    Instantiate player and enemies

--- js/app.js
+++ js/app.js
@@ -14,9 +14,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-	
-	
-	
+	this.x = 10* dt;  	
 }
 
 // Draw the enemy on the screen, required method for game
@@ -47,7 +45,12 @@ Player.prototype.handleInput = function() {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var allEnemies = [];
+for (i = 0; i < 4; i++ ) {
+	allEnemies[ i ] = new Enemy(); 
+}
 
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
